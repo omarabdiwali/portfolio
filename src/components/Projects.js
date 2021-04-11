72007073952c9c1804cd917a75978b03386fb552
omarabdiwali <63807504+omarabdiwali@users.noreply.github.com>
2021-04-11 00:32:50 -0600

    Update Projects.js

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -72,7 +72,7 @@ const projects = [
   {
     title: ["Blackjack", "https://github.com/omarabdiwali/blackjack", <GiCardJoker />],
     body: "A blackjack game that plays on your command line console. Players connect to it using a database, which stores their names and how much money they currently have. The game has the option to double, hit, and stand. The dealer stops hitting on 17, either hard or soft. It gives the option to play again, and add additional players. At the end of the game, it updates the amount of money.",
-    footer: [ "https://gihub.com/omarabdiwali/blackjack" ]
+    footer: [ "https://github.com/omarabdiwali/blackjack" ]
   },
 
   {
